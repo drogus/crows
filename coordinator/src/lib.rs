5c547258c148b4993bd4219a2086d0cc44f75531
Piotr Sarnacki <drogus@gmail.com>
2024-08-21 13:13:19 +0200

    Extract coordinator and worker entrypoints to lib

---
+++ coordinator/src/lib.rs
@@ -0,0 +1,258 @@
+use std::collections::HashMap;
+use std::sync::Arc;
+use std::time::Duration;
+
+use crows_utils::services::{
+    create_coordinator_server, create_worker_to_coordinator_server, ClientClient, CoordinatorError,
+    RunId, WorkerClient,
+};
+use crows_utils::services::{Coordinator, WorkerToCoordinator};
+use crows_utils::InfoMessage;
+use crows_wasm::fetch_config;
+use futures::future::join_all;
+use tokio::sync::{Mutex, RwLock};
+use tokio::time::sleep;
+use uuid::Uuid;
+
+#[derive(Default, Clone)]
+struct WorkerToCoordinatorService {
+    worker_name: String,
+    #[allow(dead_code)]
+    worker_id: Uuid,
+    runs: Runs,
+}
+
+#[derive(Clone)]
+struct WorkerEntry {
+    client: WorkerClient,
+    hostname: String,
+}
+
+type Runs = Arc<RwLock<HashMap<RunId, ClientClient>>>;
+
+impl WorkerToCoordinator for WorkerToCoordinatorService {
+    async fn ping(&self) -> String {
+        "OK".into()
+    }
+
+    async fn update(&self, id: RunId, update: InfoMessage) {
+        // TODO: we should remove a run if the InfoMessage is saying it's either done
+        //       or errored out
+        let runs = self.runs.read().await;
+        if let Some(client) = runs.get(&id) {
+            if let Err(_) = client
+                .update(id.clone(), self.worker_name.clone(), update)
+                .await
+            {
+                drop(runs);
+                self.runs.write().await.remove(&id);
+            }
+        }
+    }
+}
+
+#[derive(Clone)]
+struct CoordinatorService {
+    scenarios: Arc<Mutex<HashMap<String, Vec<u8>>>>,
+    workers: Arc<Mutex<HashMap<Uuid, WorkerEntry>>>,
+    runs: Runs,
+    // TODO: names like ClientClient suck, maybe it would be nice to use associated
+    // types on a trait for this?
+    client: ClientClient,
+}
+
+impl Coordinator for CoordinatorService {
+    async fn upload_scenario(
+        &self,
+        name: String,
+        content: Vec<u8>,
+    ) -> Result<(), CoordinatorError> {
+        // TODO: to send bandwidth maybe it will be worth it to gzip the data? we would be
+        // gzipping once and sending to N clients
+        //
+        // send each uploaded scenario to all of the workers
+        for (_, worker_entry) in self.workers.lock().await.iter() {
+            let mut futures = Vec::new();
+            futures.push(async {
+                // TODO: handle Result
+                if let Err(e) = worker_entry
+                    .client
+                    .upload_scenario(name.clone(), content.clone())
+                    .await {
+                        // TODO: should we send it to a client
+                        eprintln!("Error while uploading scenario to a worker: {e:?}");
+                    }
+            });
+
+            join_all(futures).await;
+        }
+        self.scenarios.lock().await.insert(name, content);
+
+        Ok(())
+    }
+
+    async fn start(
+        &self,
+        name: String,
+        workers_number: usize,
+    ) -> Result<(RunId, Vec<String>), CoordinatorError> {
+        let id = RunId::new();
+        // TODO: also this way we will always choose the same workers. in the future we should
+        // either always split between all workers or do some kind of round robin
+        // TODO: at the moment we split evenly. in the future we could get some kind of diagnostic
+        // data from workers in order to determine how much traffic can we push to each worker
+        // TODO: creating a runtime is probably fast enough, but I'd like to measure and see
+        // if it's not better to keep one around so we don't create it before each test run
+        let scenarios = self.scenarios.lock().await;
+        let scenario = scenarios
+            .get(&name)
+            .ok_or(CoordinatorError::NoSuchModule(name.clone()))?
+            .to_owned();
+        drop(scenarios);
+
+        let (runtime, _) = crows_wasm::Runtime::new(&scenario)
+            .map_err(|err| CoordinatorError::FailedToCreateRuntime(err.to_string()))?;
+        let (instance, _, mut store) = runtime
+            .new_instance()
+            .await
+            .map_err(|_| CoordinatorError::FailedToCompileModule)?;
+        let config = fetch_config(instance, &mut store)
+            .await
+            .map_err(|err| CoordinatorError::CouldNotFetchConfig(err.to_string()))?
+            .split(workers_number);
+
+        let mut runs = self.runs.write().await;
+        runs.insert(id.clone(), self.client.clone());
+        drop(runs);
+
+        let mut worker_names = Vec::new();
+        let workers = self.workers.lock().await;
+        if workers_number > workers.len() {
+            return Err(CoordinatorError::NotEnoughWorkers(workers.len()));
+        }
+
+        for (_, worker_entry) in workers.iter().take(workers_number) {
+            worker_names.push(worker_entry.hostname.clone());
+            let name = name.clone();
+            let config = config.clone();
+            let id = id.clone();
+            let client = worker_entry.client.clone();
+            tokio::spawn(async move {
+                // TODO: at the moment we split config to split the load between each of the
+                // workers, which means that if a worker dies, we will not get a full test
+                // It would be ideal if we had a way to j
+                // client.start(name.clone(), config.clone()).await;
+                if let Err(err) = client.start(name, config, id).await {
+                    eprintln!("Got an error while trying to execute a scenario: {err:?}");
+                }
+            });
+        }
+
+        Ok((id, worker_names))
+    }
+
+    async fn list_workers(&self) -> Vec<String> {
+        self.workers
+            .lock()
+            .await
+            .iter()
+            .map(|(_, data)| data.hostname.clone())
+            .collect()
+    }
+}
+
+pub async fn start_server(worker_port: usize, client_port: usize) {
+    let original_scenarios: Arc<Mutex<HashMap<String, Vec<u8>>>> = Default::default();
+    let original_workers: Arc<Mutex<HashMap<Uuid, WorkerEntry>>> = Default::default();
+
+    let scenarios = original_scenarios.clone();
+    let workers = original_workers.clone();
+    let runs: Runs = Default::default();
+
+    let runs_clone = runs.clone();
+    tokio::spawn(async move {
+        let server = create_worker_to_coordinator_server(format!("0.0.0.0:{worker_port}"))
+            .await
+            .unwrap();
+
+        loop {
+            let runs_clone = runs_clone.clone();
+            let scenarios = scenarios.clone();
+            let workers = workers.clone();
+            let create_service = |client: WorkerClient| async move {
+                let data = client.get_data().await?;
+
+                Ok(WorkerToCoordinatorService {
+                    worker_name: data.hostname,
+                    worker_id: data.id,
+                    runs: runs_clone.clone(),
+                })
+            };
+            if let Ok(client) = server.accept(create_service.clone()).await {
+                tokio::spawn(async move {
+                    println!("Worker connected");
+                    let close_receiver = client.get_close_receiver().await;
+
+                    // sent all the current scenarios to a new worker node
+                    let locked = scenarios.lock().await;
+                    for (id, content) in locked.iter() {
+                        let _ = client.upload_scenario(id.clone(), content.clone()).await;
+                    }
+                    drop(locked);
+
+                    let mut id = None;
+                    if let Ok(data) = client.get_data().await {
+                        id = Some(data.id.clone());
+                        let mut locked = workers.lock().await;
+                        locked.entry(data.id).or_insert(WorkerEntry {
+                            client: client.clone(),
+                            hostname: data.hostname,
+                        });
+                        drop(locked);
+                    }
+
+                    if let Some(r) = close_receiver {
+                        let _ = r.await;
+                    }
+
+                    // Worker is closed, let's remove it from the list
+                    if let Some(id) = id {
+                        workers.lock().await.remove(&id);
+                    }
+                });
+            } else {
+                println!("Closing");
+                break;
+            }
+        }
+    });
+
+    let scenarios = original_scenarios.clone();
+    let workers = original_workers.clone();
+    tokio::spawn(async move {
+        let server = create_coordinator_server(format!("0.0.0.0:{client_port}"))
+            .await
+            .unwrap();
+        let create_service = |client| async move {
+            Ok(CoordinatorService {
+                scenarios,
+                workers,
+                runs: runs.clone(),
+                client,
+            })
+        };
+        while let Ok(client) = server.accept(create_service.clone()).await {
+            tokio::spawn(async move {
+                // we don't send any messages for the client, so in order to not drop it
+                // (and thus disconnect), we need to wait
+                client.wait().await;
+            });
+        }
+    });
+
+    // there should be a nicer way to wait for all of the servers to shut down, but for now
+    // this is the simplest way
+    loop {
+        sleep(Duration::from_secs(1)).await;
+    }
+}
---
+++ worker/src/lib.rs
@@ -0,0 +1,123 @@
+use crows_wasm::{run_scenario, Runtime};
+use std::sync::Arc;
+use std::{collections::HashMap, time::Duration};
+use tokio::sync::RwLock;
+use tokio::time::sleep;
+use uuid::Uuid;
+
+use crows_utils::services::{
+    connect_to_worker_to_coordinator, RunId, Worker, WorkerData, WorkerError,
+    WorkerToCoordinatorClient,
+};
+
+type ScenariosList = Arc<RwLock<HashMap<String, Vec<u8>>>>;
+
+#[derive(Clone)]
+struct WorkerService {
+    scenarios: ScenariosList,
+    hostname: String,
+    #[allow(dead_code)]
+    environment: crows_wasm::Environment,
+    client: WorkerToCoordinatorClient,
+}
+
+impl Worker for WorkerService {
+    async fn upload_scenario(&self, name: String, content: Vec<u8>) {
+        self.scenarios.write().await.insert(name, content);
+    }
+
+    async fn ping(&self) -> String {
+        todo!()
+    }
+
+    async fn start(
+        &self,
+        name: String,
+        config: crows_shared::Config,
+        id: RunId,
+    ) -> Result<(), WorkerError> {
+        let locked = self.scenarios.read().await;
+        let scenario = locked
+            .get(&name)
+            .ok_or(WorkerError::ScenarioNotFound)?
+            .clone();
+        drop(locked);
+
+        let (runtime, mut info_handle) = Runtime::new(&scenario)
+            .map_err(|err| WorkerError::CouldNotCreateRuntime(err.to_string()))?;
+
+        run_scenario(runtime, config).await;
+
+        let client = self.client.clone();
+        tokio::spawn(async move {
+            while let Some(info) = info_handle.receiver.recv().await {
+                // TODO: technically we could queue the messages here and wait for the
+                // worker to try to reconnect to the coordinator with a timeout
+                // For now I prefer to just drop everything for simplicity as I'm not
+                // concerned a lot about reconnections during a scenario run
+                if let Err(_) = client.update(id.clone(), info).await {
+                    // if the client send() returns an error it means the connection is broken
+                    break;
+                }
+            }
+        });
+
+        Ok(())
+    }
+
+    async fn get_data(&self) -> WorkerData {
+        WorkerData {
+            id: Uuid::new_v4(),
+            hostname: self.hostname.clone(),
+        }
+    }
+}
+
+pub async fn connect_to_coordinator(coordinator_address: String, hostname: String) -> Result<(), Box<dyn std::error::Error>> {
+    println!("Starting with hostname: {hostname}");
+    // let handles: Vec<RuntimeHandle> = Default::default();
+    let scenarios: ScenariosList = Default::default();
+
+    loop {
+        let scenarios = scenarios.clone();
+        let hostname = hostname.clone();
+
+        println!("Connecting to {coordinator_address}");
+        let create_service_callback = |client| async move {
+            Ok(WorkerService {
+                scenarios: scenarios.clone(),
+                hostname,
+                environment: crows_wasm::Environment::new()
+                    .expect("Could not create a WASM environment"),
+                client,
+            })
+        };
+        let client = match connect_to_worker_to_coordinator(
+            coordinator_address.clone(),
+            create_service_callback,
+        )
+        .await
+        {
+            Ok(client) => client,
+            Err(e) => {
+                eprintln!("Error when trying to connect to the coordinator: {e}");
+                eprintln!("Retrying in 5 seconds");
+                sleep(Duration::from_secs(5)).await;
+                continue;
+            }
+        };
+
+        loop {
+            if let Err(_) = client.ping().await {
+                // we lost connection, break and try to reconnect
+                eprintln!(
+                    "Connection to the coordinator is broken, waiting 5s for a reconnect try"
+                );
+                sleep(Duration::from_secs(5)).await;
+                break;
+            }
+            sleep(Duration::from_secs(1)).await;
+        }
+    }
+
+}
